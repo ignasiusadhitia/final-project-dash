8337ed956cf9b0a0e4a84b6630ed70e6a56ea847
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-19 10:25:32 +0700

    feat: create InputOTPForm component

---
+++ src/components/auth/AuthBanner.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AuthBanner = () => {
+  return <div>AuthBanner</div>;
+};
+
+export default AuthBanner;
---
+++ src/components/auth/InputOTPForm.jsx
@@ -0,0 +1,59 @@
+/* eslint-disable react/jsx-curly-newline */
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+const InputOTPForm = ({
+  otp,
+  inputsRef,
+  length,
+  onInputChangeHandler,
+  onBackspaceHandler,
+  onPasteHandler,
+  onSubmitHandler,
+}) => {
+  const handlePaste = (event) => {
+    event.preventDefault();
+    const data = event.clipboardData.getData('text').slice(0, length).split('');
+    onPasteHandler(data);
+  };
+
+  return (
+    <form className="max-w-[20.625rem mx-auto]" onSubmit={onSubmitHandler}>
+      <div className="grid grid-cols-6">
+        {otp.map((value, index) => (
+          <input
+            key={index}
+            ref={(el) => (inputsRef.current[index] = el)}
+            maxLength="1"
+            type="text"
+            value={value}
+            onChange={(event) =>
+              onInputChangeHandler(event.target.value, index)
+            }
+            onKeyDown={(event) => {
+              if (event.key === 'Backspace')
+                onBackspaceHandler(event.target.value, index);
+            }}
+            onPaste={handlePaste}
+          />
+        ))}
+      </div>
+      <button type="submit">Send OTP</button>
+    </form>
+  );
+};
+
+InputOTPForm.propTypes = {
+  otp: PropTypes.string,
+  inputsRef: PropTypes.shape({
+    current: PropTypes.arrayOf(PropTypes.instanceOf(Element)),
+  }),
+  onInputChangeHandler: PropTypes.func,
+  onBackspaceHandler: PropTypes.func,
+  onPasteHandler: PropTypes.func,
+  onSubmitHandler: PropTypes.func,
+  length: PropTypes.number,
+};
+
+export default InputOTPForm;
